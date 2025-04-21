fd5466c235814d6842c9ce82bd3b4b69da229a4d
Bianca Paun <biancapaun303@gmail.com>
2025-04-21 18:08:18 +0300

    deleted unnecessary files from config server

--- config-server/src/main/resources/schema-h2.sql
+++
@@ -1,9 +0,0 @@
--- ARTIST
-CREATE TABLE artist (
-    id_artist INTEGER AUTO_INCREMENT PRIMARY KEY,
-    nume_scena VARCHAR(50) NOT NULL,
-    nume_real VARCHAR(100),
-    data_nasterii DATE,
-    tara_origine VARCHAR(50) DEFAULT 'Necunoscut'
-);
-
